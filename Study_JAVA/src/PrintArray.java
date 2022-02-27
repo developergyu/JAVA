f8c2805d932f73b42561bd73b366e1f4bf665c27
developergyu <keg1621@gmail.com>
2022-02-27 22:43:14 +0900

    Print Array

---
+++ Study_JAVA/src/PrintArray.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] iArr = {199, 95, 80, 70, 60};
+	
+		//java.util.Arrays를 추가 시켜주면 한방에 출력 가능
+		System.out.println(Arrays.toString(iArr));
+	}
+
+}
