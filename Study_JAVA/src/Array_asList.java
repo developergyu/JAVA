c50c7cefa135e0d9a62086a284d95667e3c09791
developergyu <keg1621@gmail.com>
2022-03-23 20:09:58 +0900

    asList

---
+++ Study_JAVA/src/Array_asList.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Array_asList {
+
+	public static void main(String[] args) {
+		
+		List list = Arrays.asList(1,2,3,4,5);
+		System.out.println(list);
+	}
+}
